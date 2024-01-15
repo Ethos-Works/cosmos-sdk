7b9cb451c872479505ee83ad504e01c97c258d0f
lucasege <lucasege@cs.stanford.edu>
2024-01-14 23:36:48 -0800

    Add marshal testing

--- crypto/keys/ethsecp256k1/ethsecp256k1_test.go
+++ crypto/keys/ethsecp256k1/ethsecp256k1_test.go
@@ -1,6 +1,7 @@
 package ethsecp256k1
 
 import (
+	"encoding/base64"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,6 +10,7 @@ import (
 	ethsecp256k1 "github.com/ethereum/go-ethereum/crypto/secp256k1"
 
 	// tmcrypto "github.com/cometbft/cometbft/crypto"
+	"github.com/cosmos/cosmos-sdk/codec"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 )
 
@@ -32,7 +34,8 @@ func TestPrivKeyPrivKey(t *testing.T) {
 	// validate we can sign some bytes
 	msg := []byte("hello world")
 	sigHash := ethcrypto.Keccak256Hash(msg)
-	expectedSig, _ := ethsecp256k1.Sign(sigHash.Bytes(), privKey.Key)
+	expectedSig, err := ethsecp256k1.Sign(sigHash.Bytes(), privKey.Key)
+	require.NoError(t, err)
 
 	sig, err := privKey.Sign(msg)
 	require.NoError(t, err)
@@ -59,4 +62,59 @@ func TestPrivKeyPubKey(t *testing.T) {
 
 	res := pubKey.VerifySignature(msg, sig)
 	require.True(t, res)
+}
+
+func TestMarshalAmino(t *testing.T) {
+	aminoCdc := codec.NewLegacyAmino()
+	privKey, err := GenerateKey()
+	require.NoError(t, err)
+
+	pubKey := privKey.PubKey().(*PubKey)
+
+	testCases := []struct {
+		desc      string
+		msg       codec.AminoMarshaler
+		typ       interface{}
+		expBinary []byte
+		expJSON   string
+	}{
+		{
+			"ethsecp256k1 private key",
+			privKey,
+			&PrivKey{},
+			append([]byte{32}, privKey.Bytes()...), // Length-prefixed.
+			"\"" + base64.StdEncoding.EncodeToString(privKey.Bytes()) + "\"",
+		},
+		{
+			"ethsecp256k1 public key",
+			pubKey,
+			&PubKey{},
+			append([]byte{33}, pubKey.Bytes()...), // Length-prefixed.
+			"\"" + base64.StdEncoding.EncodeToString(pubKey.Bytes()) + "\"",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			// Do a round trip of encoding/decoding binary.
+			bz, err := aminoCdc.Marshal(tc.msg)
+			require.NoError(t, err)
+			require.Equal(t, tc.expBinary, bz)
+
+			err = aminoCdc.Unmarshal(bz, tc.typ)
+			require.NoError(t, err)
+
+			require.Equal(t, tc.msg, tc.typ)
+
+			// Do a round trip of encoding/decoding JSON.
+			bz, err = aminoCdc.MarshalJSON(tc.msg)
+			require.NoError(t, err)
+			require.Equal(t, tc.expJSON, string(bz))
+
+			err = aminoCdc.UnmarshalJSON(bz, tc.typ)
+			require.NoError(t, err)
+
+			require.Equal(t, tc.msg, tc.typ)
+		})
+	}
 }
